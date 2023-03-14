8c0b8cc087c0e22a54c670eb7dc2eaae00a2f9b1
Subhan Rizki <113665663+subhanrizki@users.noreply.github.com>
2023-03-14 09:06:52 +0700

    Add files via upload

---
+++ Pertemuan1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+  {
+    string nama = "Heni";
+    string NIM = "0810231";
+    
+    int nilai1, nilai2, nilai3, nilai4;
+    float NilaiAkhir;
+    char index;
+
+      cout <<"Nama Siswa : "; >> string.nama;
+      cout <<"\tPresensi : "; string.NIM;
+      cout <<"\tPraktikum :  "; cin>>nilai2;
+      cout <<"\tUTS : "; cin>>nilai3;
+      cout <<"\tUAS: "; cin>>nilai4;
+
+    
+     //nilai akhir = presensi*10%+praktikum*20%+UTS*30%+UAS*40$+%
+     //nilai 1 - presensi, nilai 2 - praktik, nilai 3 - UTS,          //nilai 4 - UAS
+    
+      //array
+      int nilai [4];
+
+      //isi array
+      nilai [0] = 0;
+      nilai [1] = 70;
+      nilai [2] = 80;
+      nilai [3] = 90;
+      nilai [4] = 80;
+
+      //
+      
+    
+  }  
